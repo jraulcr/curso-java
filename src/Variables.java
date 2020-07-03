986fafd708a83030bad07a9f22a73394dedce3db
jraulcr <jraulcr@gmail.com>
2020-07-03 19:24:34 +0200

    Curso Java Pildorasinformaticas 03/07/2020

---
+++ src/Variables.java
@@ -0,0 +1,18 @@
+
+public class Variables {
+
+	public static void main(String args[]) {
+		
+		int edad = 50; //Declaración e inicialización devariable en una única línea
+		/*
+		 * La siguiente instrucciónimprime en la consola (System.out) el valor de la variable 'edad'
+		 * 
+		 * */
+		System.out.println(edad);
+		
+		edad = 75;
+		
+		System.out.println(edad);
+	
+	}
+}
